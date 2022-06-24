abd32f8db298639a7f6b8331894fd703c546a130
federicoCampero <fedecampa777@gmail.com>
2022-06-23 23:37:09 -0300

    inicio proyecto

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+
+function App() {
+  return (
+      <BrowserRouter>
+          <Routes>
+
+              <Route exact path="/" element={<Home />} />    
+
+
+              
+              
+          </Routes>
+      </BrowserRouter>
+
+  )
+}
+
+export default App;
